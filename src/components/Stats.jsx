6cdb196848a40e4b6b26ffee02c08843e16f5183
Steppico <steppashr@gmail.com>
2019-08-14 09:07:52 +0900

    add new stats

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -13,6 +13,24 @@ const Stats = props => {
     return count;
   };
 
+  const prefCountry = agencies => {
+    const countryHash = {};
+    agencies.map(agency =>
+      countryHash[agency.pad.location.country_code] === undefined
+        ? (countryHash[agency.pad.location.country_code] = 1)
+        : countryHash[agency.pad.location.country_code]++
+    );
+    const result = { country: "", count: 0 };
+    for (const country in countryHash) {
+      console.log("cococontry", countryHash[country]);
+      if (countryHash[country] > result.count) {
+        result.country = country;
+        result.count = countryHash[country];
+      }
+    }
+    return `${result.country} (${result.count})`;
+  };
+
   return (
     console.log(props) || (
       <div className="block__stats">
@@ -20,31 +38,40 @@ const Stats = props => {
           <>
             <h1>Stats for: {props.selectedAgency}</h1>
             <div className="block__numberLaunches">
-              <p>
+              <div className="block__launch__num">
                 Number of launches:{" "}
-                {props[props.selectedAgency.toLowerCase()].length}
-              </p>
-              <p>
+                <div className="launch__num">
+                  {props[props.selectedAgency.toLowerCase()].length}
+                </div>
+              </div>
+              <div className="block__launch__fail">
                 Failures:{" "}
-                {countFunc(
-                  props[props.selectedAgency.toLowerCase()],
-                  "Failure"
-                )}
-              </p>
-              <p>
+                <div className="launch__fail">
+                  {countFunc(
+                    props[props.selectedAgency.toLowerCase()],
+                    "Failure"
+                  )}
+                </div>
+              </div>
+              <div className="block__launch__win">
                 Successes:{" "}
-                {countFunc(
-                  props[props.selectedAgency.toLowerCase()],
-                  "Success"
-                )}
-              </p>
-              <p>
+                <div className="launch__win">
+                  {countFunc(
+                    props[props.selectedAgency.toLowerCase()],
+                    "Success"
+                  )}
+                </div>
+              </div>
+              <div className="block__successfail__ratio">
                 Success/Fail ratio:{" "}
-                {`${Math.ceil(props.Success / props.Failure)}/1`}
-              </p>
-              <p>On a graph:</p>
-              <div class_name="pie__chart">
+                <div className="successfail__ratio">
+                  {`${Math.ceil(props.Success / props.Failure)}/1`}
+                </div>
+              </div>
+              <div className="block__pie__chart">
+                On a graph:
                 <Pie
+                  className="pie__chart"
                   data={{
                     labels: ["Failures", "Successes"],
                     datasets: [
@@ -55,6 +82,12 @@ const Stats = props => {
                     ]
                   }}
                 />
+              </div>
+              <div className="block__preferredlaunch__country">
+                Preferred Launch Country:
+                <div className="preferredlaunch__country">
+                  {prefCountry(props[props.selectedAgency.toLowerCase()])}
+                </div>
               </div>
             </div>
           </>
